ccf5a63860cde2a542878bd848010e334dc3d25a
rebecca-agyei <rebecca.agyei@amalitech.org>
2021-06-25 08:33:24 +0000

    Commit of my inventory

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
 
-// const colorHandler = (quantity) => {
 
-//     switch (true) {
-//         case (quantity == 0):
-//             return "red";
-//         case (0 < quantity && quantity < 21):
-//             return "orange";
-//         case (quantity > 20):
-//             return "green";
-//         default:
-//             return "green";
-//     }
     
 // }
 // const editItem = (index) => {
@@ -60,7 +49,7 @@ let labelColor = () => {
               <td class="cat">${localStorageProducts[i].category}</td>
               <td class="num">${localStorageProducts[i].quantity}</div></td>
               <td id='label_color${i}'>Check</td>
-              <td><i class="fa fa-pencil" aria-hidden="true" id="upd_${i}" onClick="productGetUpdate(this.id)"></i> &nbsp;&nbsp; 
+              <td><i class="fa fa-pencil" aria-hidden="true" id="upd_${i}" onClick="productGetUpdate(this.id)" href="../html/stockupdate.html"></i> &nbsp;&nbsp; 
               <i id="del_${i}" class="fa fa-trash-o" aria-hidden="true" onclick="deleteModal(this.id)"></i></td>
               </tr>
       `
@@ -73,132 +62,54 @@ let labelColor = () => {
   }
   displayItems();
   
-  const updateItem = () => {
-      let noi = document.getElementById('noi');
-      let numberOfItems = (JSON.parse(localStorage.getItem("itemsInfo")).length);
-      
-      if (localStorage.getItem("itemsInfo")) {
-          noi.innerText = (JSON.parse(localStorage.getItem("itemsInfo"))).length;
-      }
-      else noi.innerText = '0';
+  const totalNumberofItems = () => {
+    let total = document.getElementById('total');
+    
+    if (localStorage.getItem("itemsInfo")) {
+        total.innerText = (JSON.parse(localStorage.getItem("itemsInfo"))).length;
+    }
+    else total.innerText = '0';
+}
+totalNumberofItems()
+
+const totalItemsInStock = () => {
+    let products = JSON.parse(localStorage.getItem("itemsInfo"));
+    let available = [];
+    if (products) {
+        products.forEach(item => {
+            if (parseInt(item.quantity) > 0) {
+                available.push(item);
+            }
+        });
+        document.getElementById("itemsInStock").innerText = available.length;
+    }
+    else {
+        document.getElementById("itemsInStock").innerText = '0';
+    }
+
+}
+totalItemsInStock()
+
+const totalNumberOfCategories = () => {
+let products = JSON.parse(localStorage.getItem("itemsInfo"));
+let distinctValues = [];
+for (i = 0; i < products.length; i++) {
+  if (!distinctValues.includes(products[i].category.toLowerCase())) {
+    distinctValues.push(products[i].category.toLowerCase());
   }
-  const countOutOfStock = () => {
-      let items = JSON.parse(localStorage.getItem("itemsInfo"));
-      let availableItems = [];
-      if (items) {
-          items.forEach(item => {
-              if (parseInt(item.quantity) > 0) {
-                  availableItems.push(item);
-              }
-          });
-          document.getElementById("itemsInStock").innerText = availableItems.length;
-      }
-      else {
-          document.getElementById("itemsInStock").innerText = '0';
-      }
+}
+
+document.getElementById("totalCategories").innerText = distinctValues.length;
+
+} 
+
+totalNumberOfCategories()
   
-  }
+   
+
+
   
-  
-  let deleteIcons = document.querySelectorAll('.iconDiv2')
-  // console.log(deleteIcons);
-  
-  let modal = document.querySelector("#delete-modal")
-  let backdrop = document.querySelector("#backdrop")
-  let cancelBtn = modal.querySelector(".btn1--passive")
-  let delBtn = modal.querySelector(".btn1--danger")
-  
-  let clickedItemIndex
-  
-  const deleteItemHandler = (index) => {
-     
-      clickedItemIndex = index
-  }
-  
-  
-  const deleteHandler = (index) => {
-      let localStorageProducts = JSON.parse(localStorage.getItem("itemsInfo"))
-      // console.log(localStorageProducts);
-      localStorageProducts.splice(index, 1);
-      localStorage.setItem('itemsInfo', JSON.stringify(localStorageProducts));
-      displayItems();
-      updateItem();
-      countOutOfStock();
-      window.location.href = "/index.html";
-  
-  
-  }
-  
-  const toggleBackdrop = () => {
-      backdrop.classList.toggle("visible")
-      modal.classList.add("visible")
-  }
-  
-  const hideBackdrop = () => {
-      modal.classList.remove("visible")
-      backdrop.classList.remove("visible")
-      // deleteHandler(clickedItemIndex)
-      // console.log(clickedItemIndex);
-  }
-  const hideBackdrop2 = () => {
-      modal.classList.remove("visible")
-      backdrop.classList.remove("visible")
-      deleteHandler(clickedItemIndex)
-      // console.log(clickedItemIndex);
-  }
-  
-  cancelBtn.addEventListener("click", hideBackdrop)
-  delBtn.addEventListener("click", hideBackdrop2)
-  backdrop.addEventListener("click", hideBackdrop)
-  
-  for(let deleteIcon of deleteIcons) {
-      deleteIcon.addEventListener("click", toggleBackdrop)
-      
-  }
-  // ToggleHandler
-  
-  let toggle = document.querySelector('.toggle');
-  // console.log(toggle);
-  function toggleMenu() {
-      let navigation = document.querySelector('.navigation');
-      let main = document.querySelector('.main');
-      toggle.classList.toggle('active')
-      navigation.classList.toggle('active')
-      main.classList.toggle('active')
-  }
-  
-  toggle.addEventListener('click', toggleMenu)
-  
-  updateItem();
-  countOutOfStock();
-  
-  
-  
-  // const addItemBtn = document.getElementById("btn-add")
-  
-  // function addItemHandler(){
-  //     var table = document.getElementById("table")
-  //     var row = document.createElement("tr")
-  //     console.log(row)
-  //     var td1 = document.createElement("td")
-  //     var td2 = document.createElement("td")
-  //     var td3 = document.createElement("td")
-  //     var td4 = document.createElement("td")
-  
-  //     td1.innerHTML = document.getElementById("name").value
-  //     td2.innerHTML = document.getElementById("description").value
-  //     td3.innerHTML = document.getElementById("category").value
-  //     td4.innerHTML = document.getElementById("quantity").value
-  
-  //     row.appendChild(td1)
-  //     row.appendChild(td2)
-  //     row.appendChild(td3)
-  //     row.appendChild(td4)
-  
-  //     table.children[0].appendChild(row)
-  
-  // }
-  
-  // document.getElementById("btn-add").addEventListener('click', addItemHandler)
+
+ 
   
   
